c8442e7f9bdb62589bc6aac84531afbb8ab25bfa
cc-cpo <codecave@gmx.net>
2019-03-08 12:40:10 +0100

    fix debug response hardcoded PUT

--- src/main/java/de/banksapi/client/services/internal/AroundRequestHandler.java
+++ src/main/java/de/banksapi/client/services/internal/AroundRequestHandler.java
@@ -23,7 +23,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public <T> Response<T> postForm(String postData, Class<T> responseClass) {
         debugRequest(new RequestDetails(HttpMethod.POST, wrapped.getRequestUrl(), postData, correlationId));
         Response<T> response = wrapped.postForm(postData, responseClass);
-        debugResponse(response);
+        debugResponse(response, HttpMethod.POST);
         return response;
     }
 
@@ -31,7 +31,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public <U, T> Response<T> post(U postData, Class<T> responseClass) {
         debugRequest(new RequestDetails(HttpMethod.POST, wrapped.getRequestUrl(), postData, correlationId));
         Response<T> response = wrapped.post(postData, responseClass);
-        debugResponse(response);
+        debugResponse(response, HttpMethod.POST);
         return response;
     }
 
@@ -39,7 +39,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public <U, T> Response<T> put(U putData, Class<T> responseClass) {
         debugRequest(new RequestDetails(HttpMethod.PUT, wrapped.getRequestUrl(), putData, correlationId));
         Response<T> response = wrapped.put(putData, responseClass);
-        debugResponse(response);
+        debugResponse(response, HttpMethod.PUT);
         return response;
     }
 
@@ -47,7 +47,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public <T> Response<T> put(Class<T> responseClass) {
         debugRequest(new RequestDetails(HttpMethod.PUT, wrapped.getRequestUrl(), null, correlationId));
         final Response<T> response = wrapped.put(responseClass);
-        debugResponse(response);
+        debugResponse(response, HttpMethod.PUT);
         return response;
     }
 
@@ -56,7 +56,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public Response<String> delete() {
         debugRequest(new RequestDetails(HttpMethod.DELETE, wrapped.getRequestUrl(), null, correlationId));
         Response<String> response = wrapped.delete();
-        debugResponse(response);
+        debugResponse(response, HttpMethod.DELETE);
         return response;
     }
 
@@ -64,7 +64,7 @@ public final class AroundRequestHandler implements IHTTPClient {
     public <T> Response<T> get(Class<T> responseClass) {
         debugRequest(new RequestDetails(HttpMethod.GET, wrapped.getRequestUrl(), null, correlationId));
         Response<T> response = wrapped.get(responseClass);
-        debugResponse(response);
+        debugResponse(response, HttpMethod.GET);
         return response;
     }
 
@@ -87,13 +87,13 @@ public final class AroundRequestHandler implements IHTTPClient {
         }
     }
 
-    private <T> void debugResponse(Response<T> response) {
+    private <T> void debugResponse(final Response<T> response, final HttpMethod method) {
         if (filter == null) {
             return;
         }
         final Instant responseTimestamp = Instant.now();
 
-        RequestDetails requestDebugDetails = new RequestDetails(HttpMethod.PUT, wrapped.getRequestUrl(), null, correlationId);
+        RequestDetails requestDebugDetails = new RequestDetails(method, wrapped.getRequestUrl(), null, correlationId);
         try {
             filter.onResponse(responseTimestamp, requestDebugDetails, response);
         } catch (final Exception ex) {
